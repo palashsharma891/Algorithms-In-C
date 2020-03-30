ce2c028a706dad5d5515e398767ea2e656ce7850
Palash Sharma <54630543+palashsharma891@users.noreply.github.com>
2020-03-30 12:11:10 +0530

    Create Ex6.c

---
+++ Chapter 1/Ex6.c
@@ -0,0 +1,24 @@
+For Algorithm 1 :
+u, v = gazillion values which would be a pain to write down here ;p
+
+For Algorithm 2 :
+u = 56789
+v = 12345
+
+u = 12345
+v = 7409
+
+u = 7409
+v = 4963
+
+u = 4963
+v = 2473
+
+u = 2473
+v = 2463
+
+u = 2463
+v = 10
+
+u = 10
+v = 3
